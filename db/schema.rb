24707ba29cc3f026dff41b9900f735dad4575c64
Loris <34445666+Lobbby@users.noreply.github.com>
2024-06-13 19:28:14 +0200

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_13_171731) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_13_172253) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "body"
@@ -20,4 +20,35 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_13_171731) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "email_verification_tokens", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_email_verification_tokens_on_user_id"
+  end
+
+  create_table "password_reset_tokens", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_password_reset_tokens_on_user_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "user_agent"
+    t.string "ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.boolean "verified", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "email_verification_tokens", "users"
+  add_foreign_key "password_reset_tokens", "users"
+  add_foreign_key "sessions", "users"
 end
